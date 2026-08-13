898cbaee9cd7a5132c8c3afc74514dcc0d351fb5
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2026-08-13 15:45:49 -0300

    Lista de Espera: brincando com java collections

--- Filtro de Convidados/src/br/com/evento/modelo/App.java
+++ Filtro de Convidados/src/br/com/evento/modelo/App.java
@@ -8,9 +8,26 @@
 // Crie um Set<String> (use HashSet) chamado convidadosConfirmados.
 // Adicione todos os nomes da lista para dentro do Set.
 // Imprima o tamanho da List original e depois o tamanho do Set final para comprovar que as duplicatas foram eliminadas!
+package br.com.evento.modelo;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        List<String> listaDeEspera = new ArrayList<>();
+        listaDeEspera.add("Rihanna");
+        listaDeEspera.add("Katy Perry");
+        listaDeEspera.add("Shakira");
+        listaDeEspera.add("Miley Cyrius");
+        listaDeEspera.add("Miley Cyrius");
+        listaDeEspera.add("Katy Perry");
+        listaDeEspera.add("Rihanna");
+
+        Set<String> convidadosConfirmados = new HashSet<>(listaDeEspera);
+
+        System.out.println(listaDeEspera);
+        System.out.println(convidadosConfirmados);
     }
 }
